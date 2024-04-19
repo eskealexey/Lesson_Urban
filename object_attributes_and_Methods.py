04aa5345c07d25dfda32bcac86cd78eeaa1d00a8
alexey <eskealexey@mail.ru>
2024-04-19 15:35:39 +0300

    исправленное задание

--- object_attributes_and_Methods.py
+++ object_attributes_and_Methods.py
@@ -4,19 +4,21 @@
 #     Задайте ему новый атрибут numberOfFloors = 10
 #     В цикле пройдитесь по атрибуту numberOfFloors и распечайте значение "Текущий этаж равен"
 #     Полученный код напишите в ответ к домашему заданию
-#     Что-то я не совсем понял в задании про цикл :(
+
 class House():
     """
     Описание класса House
     """
-    def __init__(self,numberOfFloors=10):
+    def __init__(self, numberOfFloors=10):
         self.numberOfFloors = numberOfFloors
 
     def view_floor(self):
         print(f'Текущий этаж равен {self.numberOfFloors}')
 
 
-home = House()
-home.view_floor()
-home.numberOfFloors = 5
-home.view_floor()
+home = House(10)
+# print(home.numberOfFloors)
+for i in range(1, home.numberOfFloors + 1):
+    home.numberOfFloors = i
+    home.view_floor()
+

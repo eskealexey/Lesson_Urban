eaec22e96d7360fd9074efa1e1f53f88e3ea8ea1
alexey <eskealexey@mail.ru>
2024-06-01 13:32:41 +0300

    Домашнее задание по теме "Потоки на классах"_

--- Modul_10/threads_on_classes.py
+++ Modul_10/threads_on_classes.py
@@ -1,14 +1,14 @@
 from threading import Thread
 import time
+
+
 class Knight(Thread):
     def __init__(self, name, scil, *args, **kwargs):
         super(Knight, self).__init__(*args, **kwargs)
         self.name = name
         self.scil = scil
 
-
     def run(self):
-        '''Sir Lancelot, сражается 1 день(дня)..., осталось 90 воинов.'''
         day = 0
         enemy = 100
         print(f'{self.name}, на нас напали!')
@@ -20,12 +20,10 @@ class Knight(Thread):
         print(f'{self.name}, одержал победу спустя {day} дней(дня).')
 
 
-knight1 = Knight("Sir Lancelot", 10) # Низкий уровень умения
-knight2 = Knight("Sir Galahad", 20) # Высокий уровень умения
+knight1 = Knight("Sir Lancelot", 10)
+knight2 = Knight("Sir Galahad", 20)
 knight1.start()
 knight2.start()
 knight1.join()
 knight2.join()
 print("Все битвы закончились")
-
-

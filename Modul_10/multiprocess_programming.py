5044d540c9af2a63cae2ddd36c0b5bee19a98ae4
alexey <eskealexey@mail.ru>
2024-06-12 22:29:20 +0300

    versia_2

--- Modul_10/multiprocess_programming.py
+++ Modul_10/multiprocess_programming.py
@@ -1,61 +1,46 @@
-#     Атрибут data - словарь, где ключ - название продукта, а значение - его кол-во. (изначально пустой)
-#     Метод process_request - реализует запрос (действие с товаром), принимая request - кортеж.
-#
-# Есть 2 действия: receipt - получение, shipment - отгрузка.
-# а) В случае получения данные должны поступить в data (добавить пару, если её не было и изменить значение ключа,
-# если позиция уже была в словаре)
-# б) В случае отгрузки данные товара должны уменьшаться (если товар есть в data и если товара больше чем 0).
-# 3.Метод run - принимает запросы и создаёт для каждого свой параллельный процесс, запускает его(start)
-# и замораживает(join).
 from multiprocessing import Process, Lock
 
 
 class WarehouseManager:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        self.lock = Lock()
         self.data = {}
 
     def process_request(self, request: tuple):
-        dict_tmp = {request[0]: request[2]}
-        if request[1] == 'receipt':
-            if request[0] not in self.data:
-                with lock:
-                    self.data.update(dict_tmp)
-            elif request[0] in self.data:
-                with lock:
-                    val = self.data[request[0]]
-                    self.data[request[0]] = val + request[2]
-        if request[1] == 'shipment':
-            if request[0] not in self.data or self.data[request[0]] <= 0:
-                pass
-            else:
-                with lock:
-                    val = self.data[request[0]]
-                    self.data[request[0]] = val - request[2]
+        product, action, quantity = request
+        with self.lock:
+            if action == 'receipt':
+                if product in self.data:
+                    self.data[product] += quantity
+                else:
+                    self.data[product] = quantity
+            elif action == 'shipment':
+                if product in self.data and self.data[product] >= quantity:
+                    self.data[product] -= quantity
 
     def run(self, requests):
         mang_list = []
         for i in requests:
             self.manag = Process(target=self.process_request, args=(i,))
-            mang_list.append(manager)
+            mang_list.append(self.manag)
             self.manag.start()
         for i in requests:
             self.manag.join()
 
+if __name__ == "__main__":
+    manager = WarehouseManager()
+    shared_data = manager.dict()  # Создаем общий словарь
+    lock = Lock()
 
-# Множество запросов на изменение данных о складских запасах
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 30),
-    ("product3", "receipt", 200),
-    ("product2", "shipment", 50)
-]
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
 
-lock = Lock()
-manager = WarehouseManager()
+    warehouse_manager = WarehouseManager(shared_data, lock)
+    warehouse_manager.run(requests)
 
-# Запускаем обработку запросов
-manager.run(requests)
-
-# Выводим обновленные данные о складских запасах
-print(manager.data)
+    print(shared_data)

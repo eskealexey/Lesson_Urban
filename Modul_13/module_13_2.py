44b00f79ab21d4fb00f82145bdcc1103c9c7125f
alexey <eskealexey@mail.ru>
2024-07-23 12:35:27 +0300

    Домашнее задание по теме "Хендлеры обработки сообщений"

---
+++ Modul_13/module_13_2.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config_api import API_TOKEN
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message: types.Message):
+    print(f'Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)

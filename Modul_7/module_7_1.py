1e9788a897d72d7e81f6de93002267a86c73e55e
alexey <eskealexey@mail.ru>
2024-08-23 20:15:20 +0300

    Домашнее задание по теме "Режимы открытия файлов"

--- Modul_7/module_7_1.py
+++ Modul_7/module_7_1.py
@@ -1,44 +1,42 @@
 class Product:
-
-    def __init__(self, name, weight, category):
+    def __init__(self, name: str, weigth: float, category: str):
         self.name = name
-        self.weight = weight
+        self.weight = weigth
         self.category = category
 
     def __str__(self):
-        return '{}, {}, {}'.format(self.name, self.weight, self.category)
+        return "{}, {}, {}".format(self.name, self.weight, self.category)
 
 
 class Shop:
     __file_name = 'product.txt'
-    lst_product = []
-
     def get_products(self):
         with open(file=self.__file_name, mode='r', encoding='utf-8') as f:
-            strig_ = f.read()
-            return strig_
+            list_products = f.read()
+        return list_products
 
-    def add(self, *products):
-        with open(file=self.__file_name, mode='r', encoding='utf-8') as fr:
-            vv = fr.readlines()
-            print(vv)
+    def add(self, *products: Product):
+        text = self.get_products()
 
-
-        for v in range(len(products)):
-            prod = str(products[v]).split(', ')
-            if not prod[0] in self.lst_product:
-                self.lst_product.append(prod[0])
+        for product in products:
+            if product.name in text:
+                print("{} уже есть в магазине".format(product))
+            else:
                 with open(file=self.__file_name, mode='a', encoding='utf-8') as f:
-                    f.writelines(str(products[v]) + '\n')
-
-
+                    f.write("{}\n".format(product))
 
 
 s1 = Shop()
-p1 = Product('Potati', 50.5, 'Vegetables')
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potati', 5.5, 'Vegetables')
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spagetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
 
+print(p2)
 
 s1.add(p1, p2, p3)
-print(s1.lst_product)
+
+print(s1.get_products())
+
+
+
+
